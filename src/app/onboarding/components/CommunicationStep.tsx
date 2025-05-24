1068e43520e824d978b1762315561a5e4dad6f80
McgrMik23 <124839893+McgrMik23@users.noreply.github.com>
2025-05-24 14:52:10 -0700

    communcation page

--- src/app/onboarding/components/CommunicationStep.tsx
+++ src/app/onboarding/components/CommunicationStep.tsx
@@ -10,15 +10,14 @@ import {RadioGroup, RadioGroupItem} from '@/components/ui/radio-group';
 export default function CommunicationStep() {
   const router = useRouter();
 
-  const [advice, setAdvice] = useState([0]);
-  const [connection, setConnection] = useState("");
-  const [values, setValues] = useState([0]);
+  const [engaged, setEngage] = useState([0]);
+  const [comPref, setCom] = useState([0]);
 
   const handleSubmit = (e: React.FormEvent) => { // when next button is pressed
     e.preventDefault();
 
     // TODO FOR BACKEND: STORE DATA SOMEWEHRE
-    console.log({ advice, connection, values });
+    console.log({ engaged, comPref });
 
     router.push('/home');
   };
@@ -28,62 +27,40 @@ export default function CommunicationStep() {
       {/* Advice Question */}
       <div> 
         <label htmlFor="advice" className="block text-med font-medium mb-1">
-          What kind of advice are you looking for?
+          How engaged are you hoping to be in this program?
         </label>
         <div className="flex text-s mt-4">
-          <span>Career</span>
+          <span>Very Casual</span>
           <div className='flex-1 mx-4 mt-2'>
               <Slider
               min={0}
               max={5}
               step={1}
-              value={advice}
-              onValueChange={setAdvice}
+              value={engaged}
+              onValueChange={setEngage}
               />
           </div>
-        <span>Social</span>
+        <span>Very Active</span>
         </div>
-      </div>
-
-      {/* Connection Question */}
-      <div>
-        <label htmlFor="connection" className="block text-med font-medium mb-1">
-          Who would you prefer to connect with?
-        </label>
-        <div className="flex justify-center text-med mt-4">
-          <RadioGroup defaultValue="student">
-            <div className='flex items-center space-x-2 gap-12'>
-              <div className='justify-center space-x-2'>
-              <RadioGroupItem value="student" id="r1" />
-              <Label htmlFor="r1">Student</Label>
-              </div>
-              <div className='justify-center space-x-2'>
-              <RadioGroupItem value="professional" id="r2" />
-              <Label htmlFor="r2">Industry Professional</Label>
-              </div>
-            </div>
-          </RadioGroup>
-        </div>
-        
       </div>
 
       {/* Values Question */}
       <div>
         <label htmlFor="values" className="block text-med font-medium mb-1">
-          How important is it to you that you and your match share similar values?
+        How do you prefer to communicate? <br></br>Hands-on (video calls, regular check-ins) or Chat (DMs, emails, etc.) 
         </label>
         <div className="flex text-s mt-4">
-          <span>Important</span>
+          <span className='mr-4'>Hands-on</span>
           <div className='flex-1 mx-4 mt-2'>
               <Slider
               min={0}
               max={5}
               step={1}
-              value={values}
-              onValueChange={setValues}
+              value={comPref}
+              onValueChange={setCom}
               />
           </div>
-        <span>Don't Care!</span>
+        <span className='ml-9'>Chat</span>
         </div>
       </div>
 
