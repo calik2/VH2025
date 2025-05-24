ab0493a24a4655f134269cfe90fb04106c11b237
Rhea Shetti <shettir@uci.edu>
2025-05-24 12:42:37 -0700

    updated addUser & implemented getAllUsers

--- backend/testAddUser.py
+++ backend/testAddUser.py
@@ -3,12 +3,13 @@ import requests
 url = "http://localhost:3000/api/addUser"
 
 data = {
-    "isMentor": False,
+    "isMentor": True,
+    "isStudent": True,
     "Name": "Rhea Shetti",
     "Hobbies": "coding, F1 racing",
     "Values": "work-life balance, continuous learning",
     "Preferences": {"adviceType": 0, "communicationStyle": 0, "engagement": 5, "student": True, "values": 1},
-    "LinkedIn": "https://linkedin.com/in/rheashetti"
+    "LinkedIn": "https://linkedin.com/in/rheashetti",
 }
 
 response = requests.post(url, json=data)
