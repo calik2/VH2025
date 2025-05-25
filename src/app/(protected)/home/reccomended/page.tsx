5b33e0a3dbee3f410f184e907eb9a76eeea5bf73
Cali Kuang <calik1@uci.edu>
2025-05-24 19:27:59 -0700

    WIP, idr what this does

--- src/app/(protected)/home/reccomended/page.tsx
+++ src/app/(protected)/home/reccomended/page.tsx
@@ -55,7 +55,7 @@ export default function Recommended() {
   }
 
   return (
-    <div className="container mx-auto py-10 px-4 flex flex-col items-center">
+    <div className="justify-center container mx-auto py-10 px-4 flex flex-col  items-center">
       <NavigationBar/>
       <h1 className="text-3xl font-bold mb-8">Reccomended Mentors</h1>
     
