29688d9860e58447e89316880ec89404cc373122
Rhea Shetti <shettir@uci.edu>
2025-05-24 18:04:05 -0700

    hid API keys

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBxSaCCrVDwvrRRXiOBkCwek3gTsFQZu3Q",
-  authDomain: "venushacks2025-b1cc0.firebaseapp.com",
-  projectId: "venushacks2025-b1cc0",
-  storageBucket: "venushacks2025-b1cc0.firebasestorage.app",
-  messagingSenderId: "377250989488",
-  appId: "1:377250989488:web:40b54341aedb7809738275",
-  measurementId: "G-E5Y6DEVE6H"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
---
+++ pages/api/getRecommended.js
@@ -0,0 +1,57 @@
+import { db } from "../../backend/firebaseConfig";
+import { collection, doc, getDoc, query, where, getDocs } from "firebase/firestore";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const { USER_ID } = req.query;
+
+    // get data for the user with USER_ID
+    const userRef = doc(db, "users", USER_ID);
+    const userSnapshot = await getDoc(userRef);
+    const userData = userSnapshot.data();
+    const userIsMentor = userData.isMentor;
+
+    // get all users who are NOT the same mentor/mentee type
+    const allUsersRef = collection(db, "users");
+    const oppositeUsersQuery = query(allUsersRef, where("isMentor", "!=", userIsMentor));
+
+    const querySnapshot = await getDocs(oppositeUsersQuery);
+    const allOppositeUsers = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    const scoredUsers = []
+    for(const otherUser of allOppositeUsers) {
+       const score = calculateScore(userData, otherUser);
+        scoredUsers.push({ id: otherUser.id, score });
+    }
+    scoredUsers.sort((a, b) => b.score - a.score);
+    
+    return res.status(200).json({ scoredUsers });
+    
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
+
+function calculateScore(userData, otherUser) {
+  let score = 0;
+
+  const adviceTypeScore = Math.abs(userData.Preferences.adviceType - otherUser.Preferences.adviceType) * 5;
+
+  const engagementScore = Math.abs(userData.Preferences.engagement - otherUser.Preferences.engagement) * 5;
+
+
+  const communicationScore = Math.abs(userData.Preferences.communicationStyle - otherUser.Preferences.communicationStyle) * 5;
+
+  let valueScore = 0;
+  for (const value of userData.Values) {
+    if (otherUser.Values.includes(value)) {
+      valueScore += 1;
+    }
+  }
+
+  valueScore = valueScore * (userData.Preferences.values) * 2;
+
+  score = valueScore - (adviceTypeScore + engagementScore + communicationScore);
+  return score;
+
+}
